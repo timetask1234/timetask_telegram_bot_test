097f078350dbeca17b70c27091765b7151af0407
timetask1234 <39934841+timetask1234@users.noreply.github.com>
2018-06-07 16:39:03 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,26 +14,17 @@ const getToken = (function(){
 var fcm = new FCM(serverKey);
 
 var push_data = {
-    // 수신대상
-    
-    to: clientToken,
-    // App이 실행중이지 않을 때 상태바 알림으로 등록할 내용
-    notification: {
-        title: "Hello Node",
-        body: "Node로 발송하는 Push 메시지 입니다.",
-        sound: "default",
-        click_action: "FCM_PLUGIN_ACTIVITY",
-        icon: "fcm_push_icon"
-    },
-    // 메시지 중요도
-    priority: "high",
-    // App 패키지 이름
-    restricted_package_name: "fcm.lge.com.fcm",
-    // App에게 전달할 데이터
-    data: {
-        num1: 2000,
-        num2: 3000
-    }
+ // 수신대상
+ to: clientToken,
+ // 메시지 중요도
+ priority: "high",
+ // App 패키지 이름
+ restricted_package_name: "fcm.lge.com.fcm",
+ // App에게 전달할 데이터
+ data: {
+     title: 'Registered schedule by telegram',
+     body: request.body.queryResult.fulfillmentText
+     }
 };
 
 
