70e0bc480993996a8b250d9cf017fb9b0ad40381
timetask1234 <39934841+timetask1234@users.noreply.github.com>
2018-06-07 15:27:16 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,7 +50,7 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 bot.onText(/schedule (.+)/, (msg, match) => {
 
     const chatId = msg.chat.id;
-    const resp = "서버키 : " + serverKey +"\n" + "클라이언트 토큰 : " + clientToken + "\n" + "원본 메시지 : " + msg;
+    const resp = "서버키 : " + serverKey +"\n" + "클라이언트 토큰 : " + clientToken + "\n" + "원본 메시지 : " + match;
 
     bot.sendMessage(chatId, resp);
     
