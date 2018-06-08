3bccfef3f8c76ad363f42ececdb81cdb7ab3b523
timetask1234 <39934841+timetask1234@users.noreply.github.com>
2018-06-08 15:56:47 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -69,28 +69,6 @@ var push_data = {
      }
 };
 
-var push_data = {
-    // 수신대상
-    to: client_token,
-    // App이 실행중이지 않을 때 상태바 알림으로 등록할 내용
-    notification: {
-        title: "Hello Node",
-        body: "Node로 발송하는 Push 메시지 입니다.",
-        sound: "default",
-        click_action: "FCM_PLUGIN_ACTIVITY",
-        icon: "fcm_push_icon"
-    },
-    // 메시지 중요도
-    priority: "high",
-    // App 패키지 이름
-    restricted_package_name: "study.cordova.fcmclient",
-    // App에게 전달할 데이터
-    data: {
-        num1: 2000,
-        num2: 3000
-    }
-};
-
 
 const bot = new TelegramBot(getToken(), {polling: true});
 
