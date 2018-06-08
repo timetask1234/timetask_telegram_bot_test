61005e7a2e6b9fb44d30011945ac811818a23aaf
timetask1234 <39934841+timetask1234@users.noreply.github.com>
2018-06-08 14:49:30 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,44 @@
 const TelegramBot = require('node-telegram-bot-api');
 var FCM = require('fcm-node');
 
+const projectId = 'timetask-telegram-bot';
+const sessionId = 'quickstart-session-id';
+const query = 'hello';
+const languageCode = 'en-US';
+
+const dialogflow = require('dialogflow');
+const sessionClient = new dialogflow.SessionsClient();
+
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+const request = {
+  session: sessionPath,
+  queryInput: {
+    text: {
+      text: query,
+      languageCode: languageCode,
+    },
+  },
+};
+
+sessionClient
+  .detectIntent(request)
+  .then(responses => {
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+    if (result.intent) {
+      console.log(`  Intent: ${result.intent.displayName}`);
+    } else {
+      console.log(`  No intent matched.`);
+    }
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
+
+
 var serverKey = process.env.SERVER_KEY;
 var clientToken = process.env.CLIENT_TOKEN;
 
