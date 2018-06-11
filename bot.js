cdd72a6d54736e4513052b9077e73f4fc547e780
timetask1234 <39934841+timetask1234@users.noreply.github.com>
2018-06-11 13:25:01 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-
+/*
 
 const TelegramBot = require('node-telegram-bot-api');
 const dialogflow = require('dialogflow');
@@ -87,5 +87,39 @@ bot.onText(/schedule (.+)/, (msg, match) => {
 });
 });
 
+*/
 
+const TelegramBot = require('node-telegram-bot-api');
+const getJSON = require('get-json');
+
+const getToken = (function(){
+    const token = process.env.TELEGRAM_TOKEN;
+    return function() {
+        return token;
+    };
+})();
+
+const bot = new TelegramBot(getToken(), {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+    //const chatId = msg.chat.id;
+
+    // send a message to the chat acknowledging receipt of their message
+    //bot.sendMessage(chatId, 'Received your message');
+});
 
