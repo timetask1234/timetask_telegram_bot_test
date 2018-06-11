ee6c858b9abf2260dc032ede6b42048bfe301269
timetask1234 <39934841+timetask1234@users.noreply.github.com>
2018-06-11 14:06:11 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,19 +26,7 @@ const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
 var fcm = new FCM(serverKey);
 
-var push_data = {
- // 수신대상
- to: clientToken,
- // 메시지 중요도
- priority: "high",
- // App 패키지 이름
- restricted_package_name: "fcm.lge.com.fcm",
- // App에게 전달할 데이터
- data: {
-     title: 'Registered schedule by telegram',
-     body: 'request.body.queryResult.fulfillmentText'
-     }
-};
+
 
 bot.onText(/.+/, (msg, match) => {
     
@@ -62,8 +50,34 @@ bot.onText(/.+/, (msg, match) => {
     console.log("queryText : " +result.queryText);
     console.log("Response : "  + result.fulfillmentText);
     console.log("Action : " + result.action);
-    if (result.Action == 'fcm.schedule') {
+    if (result.Action == "fcm.schedule") {
+       console.log("Come to method");
       console.log(`  Intent: ${result.intent.displayName}`);
+        
+      var push_data = {
+         // 수신대상
+       to: clientToken,
+        // 메시지 중요도
+       priority: "high",
+        // App 패키지 이름
+        restricted_package_name: "fcm.lge.com.fcm",
+        // App에게 전달할 데이터
+         data: {
+            title: 'Registered schedule by telegram',
+            body: 'request.body.queryResult.fulfillmentText'
+            }
+       };
+        
+          fcm.send(push_data, function(err, response) {
+            if (err) {
+                console.error('Push메시지 발송에 실패했습니다.');
+                console.error(err);
+                return;
+                }
+
+            console.log('Push메시지가 발송되었습니다.');
+            console.log(response);
+            });  
       
       var chatId = msg.chat.id;
       var resp = result.fulfillmentText;
@@ -80,16 +94,7 @@ bot.onText(/.+/, (msg, match) => {
     console.error('ERROR about sessionClient :', err);
   });
 
-    fcm.send(push_data, function(err, response) {
-    if (err) {
-        console.error('Push메시지 발송에 실패했습니다.');
-        console.error(err);
-        return;
-    }
 
-    console.log('Push메시지가 발송되었습니다.');
-    console.log(response);
-});
 });
 
 
