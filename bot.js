47e771162fbe533bd8965e77783f209f8fde5717
timetask1234 <39934841+timetask1234@users.noreply.github.com>
2018-06-14 09:53:24 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -176,100 +176,95 @@ bot.onText(/\/메뉴/, function(msg, match) {
   bot.sendMessage(msg.chat.id, text, keyboard);
 });
 
-bot.onText(/callback_schedule/, (msg, match) => {
-    bot.sendMessage(msg.chat.id, '일정 등록을 원하시면 예시와 같은 양식으로 써주세요.(ex: 12월 25일 일정등록, 내일 오후 1시 일정등록)');
-});
-	   
-bot.onText(/callback_whether/, (msg, match) => {
+
+bot.on('callback_query', function(msg) {
+	var data = msg.data;
+	if(data == 'callback_schedule') {
+	    //bot.sendMessage(msg.chat.id, '일정 등록을 원하시면 예시와 같은 양식으로 써주세요.(ex: 12월 25일 일정등록, 내일 오후 1시 일정등록)');
+		bot.answerCallbackQuery(msg.id, '일정 등록을 원하시면 예시와 같은 양식으로 써주세요.(ex: 12월 25일 일정등록, 내일 오후 1시 일정등록)',false);
+	}else if(data == 'callback_whether') {
 	// 기상 RSS http://www.weather.go.kr/weather/lifenindustry/sevice_rss.jsp
-    var RSS = "http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1156054000";
-    
-    // 모듈 로드
-    var client = require('cheerio-httpcli');
-    
-    // RSS 다운로드
-    client.fetch(RSS, {}, function(err, $, res) {
-      if (err) { 
-        console.log("error: "+err); return; 
-      }
-    
-      var city = $("location:nth-child(1) > city").text();
-      var date = $("channel:nth-child(1) > pubDate").text() + ' 발표';
-      bot.sendMessage(msg.chat.id, date);
-      var temp = '온도: '+$("data:nth-child(1) > temp").text()+', '+$("data:nth-child(1) > wfKor").text();
-      bot.sendMessage(msg.chat.id, temp);
-      // 필요한 항목을 추출해서 표시 ---------------------- (※1)
+	    var RSS = "http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1156054000";
 
-    });	
+	    // 모듈 로드
+	    var client = require('cheerio-httpcli');
+
+	    // RSS 다운로드
+	    client.fetch(RSS, {}, function(err, $, res) {
+	      if (err) { 
+		console.log("error: "+err); return; 
+	      }
+
+	      var city = $("location:nth-child(1) > city").text();
+	      var date = $("channel:nth-child(1) > pubDate").text() + ' 발표';
+	      bot.sendMessage(msg.chat.id, date);
+	      var temp = '온도: '+$("data:nth-child(1) > temp").text()+', '+$("data:nth-child(1) > wfKor").text();
+	      bot.sendMessage(msg.chat.id, temp);
+	      // 필요한 항목을 추출해서 표시 ---------------------- (※1)
+
+	    });	
+		bot.answerCallbackQuery(msg.id, '메모리 정보를 불러옵니다.',false);
+	}else if(data == 'callback_battery'){
+		bot.sendMessage(msg.chat.id, '베터리 정보를 불러옵니다.');
+		var push_data = {
+		// 수신대상
+		to: client_token,
+		// App이 실행중이지 않을 때 상태바 알림으로 등록할 내용
+
+		// 메시지 중요도
+		priority: "high",
+		// App 패키지 이름
+		restricted_package_name: "fcm.lge.com.fcm",
+		// App에게 전달할 데이터
+		data: {
+		    title: 'Battery information is loaded by telegram',
+		    body: 'Battery information'
+		}
+	    };
+
+	    fcm.send(push_data, function(err, response) {
+		//console.error('Push메시지 발송 시도.');
+		if (err) {
+		    console.error('Push메시지 발송에 실패했습니다.');
+		    console.error(err);
+		    return;
+		}
+
+		console.log('Push메시지가 발송되었습니다.');
+		console.log(response);
+	    });
+		bot.answerCallbackQuery(msg.id, '메모리 정보를 불러옵니다.',false);
+	}else if(data == 'callback_memory'){
+		var push_data = {
+		// 수신대상
+		to: client_token,
+		// App이 실행중이지 않을 때 상태바 알림으로 등록할 내용
+
+		// 메시지 중요도
+		priority: "high",
+		// App 패키지 이름
+		restricted_package_name: "fcm.lge.com.fcm",
+		// App에게 전달할 데이터
+		data: {
+		    title: 'Memory information is loaded by telegram',
+		    body: 'Memory information'
+		}
+	    };
+
+	    fcm.send(push_data, function(err, response) {
+		//console.error('Push메시지 발송 시도.');
+		if (err) {
+		    console.error('Push메시지 발송에 실패했습니다.');
+		    console.error(err);
+		    return;
+		}
+
+		console.log('Push메시지가 발송되었습니다.');
+		console.log(response);
+	    });
+		bot.answerCallbackQuery(msg.id, '메모리 정보를 불러옵니다.',false);
+	}
 });
-
-bot.onText(/callback_battery/, (msg, match) => {
-	bot.sendMessage(msg.chat.id, '베터리 정보를 불러옵니다.');
-	var push_data = {
-        // 수신대상
-        to: client_token,
-        // App이 실행중이지 않을 때 상태바 알림으로 등록할 내용
-
-        // 메시지 중요도
-        priority: "high",
-        // App 패키지 이름
-        restricted_package_name: "fcm.lge.com.fcm",
-        // App에게 전달할 데이터
-        data: {
-            title: 'Battery information is loaded by telegram',
-            body: 'Battery information'
-        }
-    };
-    
-    fcm.send(push_data, function(err, response) {
-        //console.error('Push메시지 발송 시도.');
-        if (err) {
-            console.error('Push메시지 발송에 실패했습니다.');
-            console.error(err);
-            return;
-        }
-     
-        console.log('Push메시지가 발송되었습니다.');
-        console.log(response);
-    });
-});
-
-bot.onText(/callback_memory/, (msg, match) => {
-	bot.sendMessage(msg.chat.id, '메모리 정보를 불러옵니다.');
-	var push_data = {
-        // 수신대상
-        to: client_token,
-        // App이 실행중이지 않을 때 상태바 알림으로 등록할 내용
-
-        // 메시지 중요도
-        priority: "high",
-        // App 패키지 이름
-        restricted_package_name: "fcm.lge.com.fcm",
-        // App에게 전달할 데이터
-        data: {
-            title: 'Memory information is loaded by telegram',
-            body: 'Memory information'
-        }
-    };
-
-    fcm.send(push_data, function(err, response) {
-        //console.error('Push메시지 발송 시도.');
-        if (err) {
-            console.error('Push메시지 발송에 실패했습니다.');
-            console.error(err);
-            return;
-        }
-     
-        console.log('Push메시지가 발송되었습니다.');
-        console.log(response);
-    });
-});
-
-bot.onText(/날씨/, (msg, match) => {
-  
-});
-
-bot.on('callback_schedule', function(msg) {bot.answerCallbackQuery(msg.id, 'Youhit a button!',false);});
 
 bot.onText(/.+/, (msg, match) => {
    var result;
