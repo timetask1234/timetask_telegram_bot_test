945d0010ba6c7111c7273f805be2717fbcd1b04f
timetask1234 <39934841+timetask1234@users.noreply.github.com>
2018-06-14 14:51:52 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,7 +65,7 @@ bot.on('callback_query', function(msg) {
 		console.log("error: "+err); return; 
 	      }
 
-	      var city = $("location:nth-child(1) > city").text();
+	      var city = $("item:nth-child(1) > category").text();
 	      var date = $("channel:nth-child(1) > pubDate").text() + ' 발표';
 	      console.log("City: " + city);
               console.log("Date: " + date);
